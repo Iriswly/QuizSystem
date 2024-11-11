f94b059953d8ae6d0bcc76e2e2d8c05e960a0a60
= <=>
2024-11-11 16:23:27 +0800

    merge the test2 to test1

--- src/Feature1.java
+++ src/Feature1.java
@@ -1,6 +1,13 @@
 public class Feature1 {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        printNumbers();
+    }
+
+    public static void printNumbers() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
 
