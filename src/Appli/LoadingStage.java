f3e9efeff3a60e1e9df8cdc816e6249a7430a6cc
tokisaka23 <tokisaka23@outlook.com>
2024-11-14 23:46:45 +0800

    Add JavaFX libraries

---
+++ src/Appli/LoadingStage.java
@@ -0,0 +1,16 @@
+package Appli;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoadingStage extends Application {
+
+    public void start(Stage stage) {
+
+    }
+}
